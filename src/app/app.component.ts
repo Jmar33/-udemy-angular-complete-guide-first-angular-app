b82594044aa51d44a6a2fce78b4c320c64544f8f
Jmar33 <josimarferreira922@yahoo.com.br>
2024-12-07 11:40:18 -0300

    feat: create a task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,27 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from "./components/user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./components/task/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
+  selectedUserId = 'u1'
+
+  get selectedUser() {
+    return this.users.find((user) => user.id  === this.selectedUserId)!
+  }
+
 
   onSelectedUser(id: string){
-    console.log('Selected user with id: ' + id);
+    this.selectedUserId = id;
   }
 
 }
