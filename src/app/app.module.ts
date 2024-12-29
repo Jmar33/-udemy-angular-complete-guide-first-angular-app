6a4ddf8c05be7f44215816b296bdc863e6ecdbd0
Jmar33 <josimarferreira922@yahoo.com.br>
2024-12-29 11:22:45 -0300

    feat: working with modules instead standalone components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
 import { BrowserModule } from "@angular/platform-browser";
+
+import { TasksModule } from "./tasks/tasks.module";
+import { SharedModule } from "./shared/shared.module";
+import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [ BrowserModule, HeaderComponent, UserComponent, TasksComponent]
+  imports: [ BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule{
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, output } from '@angular/core';
 import { type User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 
 // type User = {
 //   name: string,
@@ -11,10 +10,8 @@ import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [CardComponent]
 })
 export class UserComponent {
 
