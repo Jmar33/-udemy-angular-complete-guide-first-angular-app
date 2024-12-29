ca657125c1e2a118312be2a21cc662602480c380
Jmar33 <josimarferreira922@yahoo.com.br>
2024-12-29 06:51:12 -0300

    feat: handling with form submit

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../../dummy-tasks';
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -31,6 +32,18 @@ export class TasksComponent {
   }
 
   onCancel(){
+    this.isAddingTask = false;
+  }
+
+  onAddTask(task: NewTaskData){
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: task.title,
+      summary: task.summary,
+      dueDate: task.date
+    })
+
     this.isAddingTask = false;
   }
 }
